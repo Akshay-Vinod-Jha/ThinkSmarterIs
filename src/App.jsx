59ec12b8307d372ac1ffe250de0de4bda16a8793
AdityaVyavhare <vyavhareaditya11@gmail.com>
2024-02-29 10:51:38 +0530

    29-02-2024 10:51 by Aditya

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import SpellChecker from "./FolderForTools/SpellChecker/SpellChecker";
+// import React from "react";
+import SignUp from "./Signup&login/SignUp";
+// import SpellChecker from "./FolderForTools/SpellChecker/SpellChecker";
 function App() {
   const mainContainer =
     "w-screen h-auto bg-[#080b10] p-1 md:p-4 grid grid-cols-1 place-content-center place-items-center";
   return (
     <div className={mainContainer}>
-      <SpellChecker></SpellChecker>
+      <SignUp />
     </div>
   );
 }
