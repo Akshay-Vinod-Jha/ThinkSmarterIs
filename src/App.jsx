f0ce4a69270dd56c1569cd2b2561bc1e7d1d3f12
Akshay Jha <akshayjha2006@gmail.com>
2024-02-29 16:31:36 +0530

    aj 4:31 29-2

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ function App() {
   const mainContainer =
     "w-screen h-auto bg-[#080b10]  grid grid-cols-1 place-content-center place-items-center";
   return (
-    <div className={mainContainer}>
-      <TextToImage></TextToImage>
-    </div>
+    <div className={mainContainer}>{/* <TextToImage></TextToImage> */}</div>
   );
 }
 
