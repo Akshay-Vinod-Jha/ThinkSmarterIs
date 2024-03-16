1179af9ee491ad4450ff0fc16b2d5784691ee5ab
Akshay Jha <akshayjha2006@gmail.com>
2024-03-16 21:35:54 +0530

    akshay  last commit 16-3-24

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import SliderCantainer from "./layout/slider/SliderCantainer";
 import Header from "./layout/Header";
 import Home from "./layout/Home/Home";
 import Footer from "./layout/Footer";
-import SignUp from "./Signup&login/SignUp";
+// import SignUp from "./Signup&login/SignUp";
 import TextToImage from "./FolderForTools/textToImage/TextToImage";
-import SignIN from "./Signup&login/SignIN";
+// import SignIN from "./Signup&login/SignIN";
 import VisionVerbalizer from "./FolderForTools/VisionVerbalizer/VisionVerbalizer";
 import SpellChecker from "./FolderForTools/SpellChecker/SpellChecker";
 import { createPortal } from "react-dom";
 import PopUp from "./UI/PopUp";
 import { useSelector } from "react-redux";
 import { getPopUpState } from "./store/popupSlice";
-import TextExtraction from "./FolderForTools/TextExtraction/TextExtraction";
+// import TextExtraction from "./FolderForTools/TextExtraction/TextExtraction";
 import QA from "./FolderForTools/QA/QA";
 import Summarizer from "./FolderForTools/Summarizer/Summarizer";
 import Chatbot from "./FolderForTools/ChatBot/Chatbot";
@@ -29,7 +29,7 @@ const App = () => {
           <PopUp content={showPop.style} />,
           document.getElementById("popup")
         )}
-      <SignIN />
+      <TTS />
     </div>
   );
 };
